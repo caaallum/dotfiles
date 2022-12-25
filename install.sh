daa17a944c5633fc3cd4c96f250f261512cfcede
Callum <54590992+caaallum@users.noreply.github.com>
2022-12-25 12:59:06 +0000

    Update installs cript

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 CONFIG=~/.config
+CONFIG_DIRS=(alacritty bspwm dunst polybar rofi sxhkd)
+DEPENDENCIES=x11-misc/sddm \
+             gui-libs/display-manager-init \
+             x11-terms/alacritty \
+             x11-wm/bspwm \
+             x11-misc/dunst \
+             x11-misc/polybar \
+             x11-misc/rofi \
+             x11-misc/sxhkd \
+             media-gfx/feh 
 
 create_link() {
     ln -s $1 $2
@@ -11,14 +21,21 @@ create_link_config() {
     create_link "$DIR/.config/$1" "$CONFIG/$1"
 }
 
+sudo emerge -a $DEPENDENCIES
+
+# Setup display manager
+sudo cp $DIR/sddm.conf /etc/
+sudo mkdir -p /etc/sddm/scripts
+sudo cp $DIR/Xsetup /etc/sddm/scripts/
+sudo chmod a+x /etc/sddm/scripts/*
+sudo sed -i -e "s/xdm/sddm/" /etc/conf.d/display-manager
+sudo rc-update add display-manager default
+
+# Install config
 mkdir -p $CONFIG
-
-CONFIG_DIRS=("alacritty" "bspwm" "dunst" "polybar" "rofi" "sxhkd")
-
-for i in ${CONFIG_DIRS[@]}
+for CONFIG_DIR in ${CONFIG_DIRS[@]}
 do
-    echo $i
+    create_link_config $CONFIG_DIR
 done
 
 create_link "$DIR"/.bashrc ~/.bashrc
-
