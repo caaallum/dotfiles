57d3f997dac04c718d6be5397277bf98736902d1
caaallum <callum_speirs@protonmail.com>
2022-12-30 15:38:01 +0000

    Update config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,9 +3,9 @@
 killall -q polybar
 
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+  for m in $(xrandr -q | grep -w connected | cut -d " " -f1); do
+    MONITOR=$m polybar --reload topbar &
   done
 else
-  polybar --reload example &
+  polybar --reload topbar &
 fi
